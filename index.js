ce12fd2e5b0ca82ffa0ea0d94b88fc2e70456ff0
HyCute <hycute.ts@gmail.com>
2024-05-11 22:00:10 +0600

    setup mongodb and create a post req for getting all blogs data

--- index.js
+++ index.js
@@ -5,12 +5,55 @@ const port = process.env.PORT || 3011
 require('dotenv').config()
 
 
+//data key
+const username = process.env.User_name;
+const password = process.env.Password;
+
+console.log(username, password);
+
 
 //middleware
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:5173']
+}))
 app.use(express.json())
 
 
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${username}:${password}@cluster0.zukg64l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const blogsCollection = client.db('blogsData').collection('user_blogs')
+
+    app.post("/blogs", async(req, res)=>{
+        const blog = req.body;
+        const result = await blogsCollection.insertOne(blog)
+        res.send(result)
+    })
+
+
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
