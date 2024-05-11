663816560fd658d9497ef89830ad22d2955e40a0
HyCute <hycute.ts@gmail.com>
2024-05-11 22:01:42 +0600

    minor mistake: create post req for add blog data

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function run() {
         res.send(result)
     })
 
-
+//
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // // Send a ping to confirm a successful connection
