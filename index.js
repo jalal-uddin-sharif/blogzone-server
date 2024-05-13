f7206f78ec6f997faa6d47d4e3cb2a965c8e7284
HyCute <hycute.ts@gmail.com>
2024-05-14 03:07:45 +0600

    jwt implement

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = process.env.PORT || 3011
-require('dotenv').config()
-
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3011;
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 //data key
 const username = process.env.User_name;
@@ -11,16 +12,33 @@ const password = process.env.Password;
 
 console.log(username, password);
 
-
 //middleware
-app.use(cors({
-    origin: ['http://localhost:5173']
-}))
-app.use(express.json())
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://blogzone-bf45b.web.app"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
 
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  if(!token) return res.status(401).send({message: 'unauthorized access'})
+  if (token) {
+    jwt.verify(token, process.env.API_SECRET_KEY, (err, decoded) => {
+      if (err) {
+        console.log(err);
+        return res.status(401).send({message: 'unauthorized access'})
+        
+      }
+      req.user = decoded;
+      next()
+    });
+  }
+};
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${username}:${password}@cluster0.zukg64l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,94 +47,137 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
 
 async function run() {
   try {
-    const blogsCollection = client.db('blogsData').collection('user_blogs')
-    const commentsCollection = client.db('blogsData').collection('user_comments')
-    const wishListCollection = client.db('blogsData').collection('user_wishlist')
+    const blogsCollection = client.db("blogsData").collection("user_blogs");
+    const commentsCollection = client
+      .db("blogsData")
+      .collection("user_comments");
+    const wishListCollection = client
+      .db("blogsData")
+      .collection("user_wishlist");
 
-    app.post("/blogs", async(req, res)=>{
-        const blog = req.body;
-        const result = await blogsCollection.insertOne(blog)
-        res.send(result)
-    })
+    //jwt token
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
 
-    app.get("/blogs", async(req, res)=>{
+      const token = jwt.sign(user, process.env.API_SECRET_KEY, {
+        expiresIn: "1h",
+      });
+      res.cookie("token", token, cookieOptions).send({ success: true });
+    });
+
+    //clearing Token
+    app.get("/logout", async (req, res) => {
+      res
+        .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+        .send({ success: true });
+    });
+
+    app.post("/blogs", async (req, res) => {
+      const blog = req.body;
+      const result = await blogsCollection.insertOne(blog);
+      res.send(result);
+    });
+
+    app.get("/blogs", async (req, res) => {
       const filter = req.query.category;
       const searchBlog = req.query.search;
       let query = {
-        title: {$regex: searchBlog, $options: 'i'},
-      }
-      if(filter) query.category = filter;
-      const result = await blogsCollection.find(query).toArray()
-      res.send(result)
-    })
+        title: { $regex: searchBlog, $options: "i" },
+      };
+      if (filter) query.category = filter;
+      const result = await blogsCollection.find(query).toArray();
+      res.send(result);
+    });
 
-    app.get("/blog/:id", async(req, res)=>{
-      const id = {_id: new ObjectId(req.params.id)}
-      const result = await blogsCollection.findOne(id)
-      res.send(result)
-    })
+    app.get("/blog/:id", async (req, res) => {
+      const id = { _id: new ObjectId(req.params.id) };
+      const result = await blogsCollection.findOne(id);
+      res.send(result);
+    });
 
     //update blogs
-    app.put("/update-blogs/:id", async(req, res)=>{
+    app.put("/update-blogs/:id", async (req, res) => {
       const blogData = req.body;
-      const query = {_id: new ObjectId(req.params.id)}
-      const options = {upsert: true}
+      const query = { _id: new ObjectId(req.params.id) };
+      const options = { upsert: true };
       const updateBlog = {
         $set: {
           ...blogData,
         },
-      }
-      const result = await blogsCollection.updateOne(query, updateBlog, options)
-      res.send(result)
-    })
+      };
+      const result = await blogsCollection.updateOne(
+        query,
+        updateBlog,
+        options
+      );
+      res.send(result);
+    });
 
     //recent blogs
-    app.get("/recent-blogs", async(req, res)=>{
-      const result = await blogsCollection.find().sort({timestamp: -1}).toArray()
-      res.send(result)
-    })
+    app.get("/recent-blogs", async (req, res) => {
+      const result = await blogsCollection
+        .find()
+        .sort({ timestamp: -1 })
+        .toArray();
+      res.send(result);
+    });
 
     //create comments
-    app.post("/send-comments", async(req, res)=>{
+    app.post("/send-comments", async (req, res) => {
       const comment = req.body;
-      const result = await commentsCollection.insertOne(comment)
-      res.send(result)
-    })
+      const result = await commentsCollection.insertOne(comment);
+      res.send(result);
+    });
 
     //get all comment
-    app.get("/comments/:id", async(req, res)=>{
-      console.log(req.params.id);
-      const result = await commentsCollection.find({id: req.params.id}).toArray()
-      res.send(result)
-    })
+    app.get("/comments/:id", async (req, res) => {
+      const result = await commentsCollection
+        .find({ id: req.params.id })
+        .toArray();
+      res.send(result);
+    });
 
     //add to wishlist
-    app.post("/wishlist", async(req, res)=>{
+    app.post("/wishlist", async (req, res) => {
       const listData = req.body;
-      console.log(listData);
-      const result = await wishListCollection.insertOne(listData)
-      res.send(result)
-    })
+      const result = await wishListCollection.insertOne(listData);
+      res.send(result);
+    });
 
     //get from wishlist
-    app.get("/wishlist/:email", async(req, res)=>{
-      const result = await wishListCollection.find({email: req.params.email}).toArray()
-      res.send(result)
-    })
+    app.get("/wishlist/:email", verifyToken, async (req, res) => {
+      const token = req.user.email
+      if(token !== req.params.email){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      const result = await wishListCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
 
     //delete wishlist data
-    app.delete("/delete-wishlist", async(req, res)=>{
+    app.delete("/delete-wishlist", async (req, res) => {
       // const email = req.query.email
-      const email = req.query.email
-      const id = req.query.id
-      const result = await wishListCollection.deleteOne({email: email, id: id})
-      res.send(result)
-    })
+      const email = req.query.email;
+      const id = req.query.id;
+      const result = await wishListCollection.deleteOne({
+        email: email,
+        id: id,
+      });
+      res.send(result);
+    });
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // // Send a ping to confirm a successful connection
@@ -129,16 +190,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", async (req, res) => {
+  res.send("server responses");
+});
 
-
-
-
-
-
-app.get('/', async(req, res)=>{
-    res.send('server responses')
-})
-
-app.listen(port, () =>{
-    console.log(`survice running on port : ${port}`);
-})
+app.listen(port, () => {
+  // console.log(`survice running on port : ${port}`);
+});
